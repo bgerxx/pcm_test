bfeded5f18320d7bc40d710d0eb0ad3a18b09c30
Benyu Xu <benyux.xu@intel.com>
2017-03-09 04:14:10 -0500

    pcm-unit: add ASM file for setjmp case

--- pcm-unit/driver/setjmp.c
+++ pcm-unit/driver/setjmp.c
@@ -1,7 +1,19 @@
 
-#include <longjmp.h>
+//#include <longjmp.h>
+//#include <setjmp.h>
 
+#include "linux/kernel.h"
 #include "test_func.h"
+
+
+typedef struct jmp_buf_tag {
+    long int regs[8];
+} jmp_buf[1];
+
+extern int setjmp (struct jmp_buf_tag env[1]);
+extern void longjmp (struct jmp_buf_tag env[1], int val)
+     __attribute__ ((__noreturn__));
+
 
 
 void test_setjmp(void)
